82b8e95eac05b0dfc4e6ef1561eac7ce3ef97e4e
gao <igao107@163.com>
2025-04-18 13:27:15 +0800

    project init

---
+++ src/main/java/org/serendipity/Main.java
@@ -0,0 +1,12 @@
+package org.serendipity;
+
+/**
+ * @author Serendipity
+ * @description
+ * @date 2025-04-18 13:22
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("args = " );
+    }
+}
